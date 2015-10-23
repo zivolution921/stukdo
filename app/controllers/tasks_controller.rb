ddd8ce9f31f4207a4958db6a1daf13cdb03d7e61
ziv <ziv921@yahoo.com>
2015-10-23 15:29:24 -0400

    Having problems adding task state

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -43,6 +43,6 @@ class TasksController < ApplicationController
     end
 
     def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content, :state)
     end
 end
