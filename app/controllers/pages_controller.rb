05e07a00f2848b6d3acc39e650de33856b03f383
ziv <ziv921@yahoo.com>
2015-10-26 00:31:35 -0400

    got stuck with an error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,8 @@
-class PagesController < ApplicationController
-  def home
-  end
+def test
+end
 
-  def about
-  end
-
-  def test 
-  end
+def home
+	if current_user
+			redirect_to tasks_path
+	end
 end
