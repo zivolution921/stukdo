a778f541adccbc737d8729bfc672a5629c4eff02
ziv <ziv921@yahoo.com>
2015-10-26 17:07:48 -0400

    Prepared to launch into heroku and fixed minor mistakes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026170020) do
+ActiveRecord::Schema.define(version: 20151023183122) do
 
   create_table "tasks", force: true do |t|
     t.text     "content"
